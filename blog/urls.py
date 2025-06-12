55a267ef7e0f2ad73f19683171ff30138b6ce630
sorenagorgan <hasanhasanhaji@gmail.com>
2025-06-12 18:40:35 +0330

    add dynamic urls
    add dynamic urls to
    - blog
    -website

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from .views import *
 
+app_name = 'blog'
 urlpatterns = [
-    path('', blog_view),
-    path('single', single_view)
+    path('', blog_view, name='blog'),
+    path('single', single_view, name='single')
 ]
