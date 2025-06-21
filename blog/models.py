35ab005ec09264eaebe9ffc3007aee981dea6bfd
sorenagorgan <hasanhasanhaji@gmail.com>
2025-06-21 21:12:34 +0330

    update model fileds
    update models of Post and contact

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=256)
+    content = models.TextField()
+    counted_view = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField()
