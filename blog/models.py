700a472f905b49443a95e6024d13624f2f5c8191
hasanhasanhaji <110488526+hasanhasanhaji@users.noreply.github.com>
2025-10-31 19:56:50 +0330

    add author field to the db
    add author field to the db

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=256)
+    author = models.ForeignKey(User, on_delete= models.CASCADE, null= True) ## add this field when teaching  اضافه کردن فیلد نویسنده
     content = models.TextField()
     counted_view = models.IntegerField(default=0)
     status = models.BooleanField(default=False)
